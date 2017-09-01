bcda0c7418f63b4978d47a1940ad7a5ac048326e
alejandra <aleja.20012010@gmail.com>
2017-08-31 20:36:07 -0500

    Margenes

--- app/src/main/java/com/i012114/taller/MainActivity.java
+++ app/src/main/java/com/i012114/taller/MainActivity.java
@@ -1,8 +1,12 @@
 package com.i012114.taller;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,11 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton mop =(FloatingActionButton) findViewById(R.id.fab_1);
+        registerForContextMenu(mop);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        menu.setHeaderTitle("Mis opciones");
+        inflater.inflate(R.menu.menu, menu);
     }
 }
